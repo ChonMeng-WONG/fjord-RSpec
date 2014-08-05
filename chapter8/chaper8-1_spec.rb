2dd82664ae09a1f2739b1a127e929eff607aeeed
ChonMeng-WONG <toshiaki1989@gmail.com>
2014-08-05 13:07:10 +0900

    修正

--- chapter8/chaper8-1_spec.rb
+++ chapter8/chaper8-1_spec.rb
@@ -1,23 +1,20 @@
 require_relative '/Users/Ko_Toshiaki/fjord-ruby/8-method/homework1'
 
-describe ask question, "yes/noで答えているかどうか" do
+describe 'ask' do
   describe goodAnswer, "yes/noで答えたらgoodAnswerがtrueを返す" do
-    before do
-      reply = gets.comp.downcase
-    end
-
+    
     it "yesで答えたらgoodAnswerはtrueを返してくる" do
-      reply == 'yes'
+      $reply == 'yes'
       expect(goodAnswer).to be_true
     end
 
     it "noで答えたらgoodAnswerはtrueを返してくる" do
-      reply == 'no'
+      $reply == 'no'
       expect(goodAnswer).to be_true
     end
 
     it "それ以外はfalseを返してくる"do
-      reply == 'neither'
+      $reply == 'neither'
       expect(goodAnswer).to be_false
     end
   end
