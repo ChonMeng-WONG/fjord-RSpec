126650e2a00aaf1f7e85c91271d44044cfe38e90
ChonMeng-WONG <toshiaki1989@gmail.com>
2014-08-05 15:16:14 +0900

    green

--- chapter8/chaper8-1_spec.rb
+++ chapter8/chaper8-1_spec.rb
@@ -1,21 +1,21 @@
 require_relative '/Users/Ko_Toshiaki/fjord-ruby/8-method/homework1'
 
-describe 'ask' do
+describe "ask" do
   describe goodAnswer, "yes/noで答えたらgoodAnswerがtrueを返す" do
-    
+
     it "yesで答えたらgoodAnswerはtrueを返してくる" do
-      $reply == 'yes'
-      expect(goodAnswer).to be_true
+      reply.stub!(:gets){"yes"}  # replyがyesと返してきたら
+      expect(goodAnswer).to be_true  # goodAnswerがtrueになる
     end
 
     it "noで答えたらgoodAnswerはtrueを返してくる" do
-      $reply == 'no'
-      expect(goodAnswer).to be_true
+      reply.stub!(:gets){"no"} # replyがnoと返してきたら
+      expect(goodAnswer).to be_true # goodAnswerがtrueになる
     end
 
     it "それ以外はfalseを返してくる"do
-      $reply == 'neither'
-      expect(goodAnswer).to be_false
+      reply.stub!(:gets){"neither"} # replyがyes/no以外と返してきたら
+      expect(goodAnswer).to be_false # goodAnswerがfalseになる
     end
   end
 end
